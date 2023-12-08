fbf158a137b76ea7b02bc102883f988c91d0c1e7
chamsabd <chamsabdelwahed42@gmail.com>
2023-12-08 02:30:35 +0100

    btn qui bouge

--- script.js
+++ script.js
@@ -17,18 +17,57 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 });
 
-
+let lastTheme = localStorage.getItem('lasttheme')?localStorage.getItem('lasttheme'):''
 
 document.addEventListener('DOMContentLoaded', function() {
+
+
+   
     
     const themes = ['XMAS','light', 'halloween'];
-
+   
     applyRandomTheme();
 
  
 
     function applyRandomTheme() {
-        const randomTheme = themes[Math.floor(Math.random() * themes.length)];
+        let randomTheme;
+console.log(lastTheme);
+        // Keep selecting a new random theme until it is different from the last one
+        do {
+            randomTheme = themes[Math.floor(Math.random() * themes.length)];
+        } while (randomTheme === lastTheme);
         document.documentElement.setAttribute('data-theme', randomTheme);
+       localStorage.setItem('lasttheme',randomTheme);
+    
     }
+
+
+
+    const button = document.getElementById('animatedBtn');
+    tr=30;
+    button.addEventListener('mouseover', function () {
+
+        
+        // Replace this condition with your specific condition
+       
+            
+
+
+
+        
+        button.style.left = `${Math.ceil(Math.random() * 90)}% `;
+         button.style.top = `${Math.ceil(Math.random() * 90)}% `;
+
+
+
+
+    });
+
+    button.addEventListener('click', function () {
+        alert('you clicked me WaaW!')
+    })
+
 });
+
+
